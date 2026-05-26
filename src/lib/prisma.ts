fa7597fd28c821f682bdeacd90aded19ecfe3774
PreetiKumari2005 <preeti20062005@gmail.com>
2026-05-26 17:51:15 +0530

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ 
-    connectionString: process.env.DATABASE_URL! 
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
   });
   return new PrismaClient({ adapter });
 }
